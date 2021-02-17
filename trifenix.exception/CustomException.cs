d170a48caa0a2061fac342005e7c39069c8c394e
FranciscoT <francisco.beltrami@trifenix.io>
2021-02-17 14:43:42 -0300

    docv2

--- trifenix.exception/CustomException.cs
+++ trifenix.exception/CustomException.cs
@@ -4,21 +4,37 @@ using System.Runtime.Serialization;
 namespace trifenix.exception
 {
     /// <summary>
-    /// Clase encargada de generar un exception personalizado
+    /// Clase encargada de generar mensaje de excepcion
     /// </summary>
     [Serializable]
     public class CustomException : Exception
     {
+        /// <summary>
+        /// Constructor de mensaje de excepcion
+        /// </summary>
         public CustomException()
         {
         }
-
+        /// <summary>
+        /// Funcion encargada mostrar mensaje especifico
+        /// </summary>
+        /// <param name="mensaje">mensaje custom</param>
         public CustomException(string mensaje) : base(mensaje)
         {
         }
+        /// <summary>
+        /// Funcion encargada mostrar mensaje especifico
+        /// </summary>
+        /// <param name="message">mensaje custom</param>
+        /// <param name="innerException"></param>
         public CustomException(string message, Exception innerException) : base(message, innerException)
         {
         }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="info"></param>
+        /// <param name="context"></param>
         protected CustomException(SerializationInfo info, StreamingContext context)
             : base(info, context)
         {
